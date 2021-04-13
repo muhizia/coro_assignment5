1a58ba3fc00e998ed39b52ca4400e89e1a63695b
Muhizi Aristide <muhizi@Muhizis-MacBook-Pro.local>
2021-04-13 02:42:12 +0200

    adding a reading in and output file

--- src/amuhiziApplication.cpp
+++ src/amuhiziApplication.cpp
@@ -1,179 +1,286 @@
-/* 
-  Example use of openCV to compute the inverse perspective transformation from a camera model
-  -------------------------------------------------------------------------------------------
-   
-  This application reads two lines from an input file cameraInvPerspectiveMonocular.txt.
-  Each line comprise a filename for files containing, respectively:
+/*******************************************************************************************************************
+*   Assignment 5: vision-based pick-and-place program for a LynxMotion AL5D robot arm to stack three bricks
+*   ----------------------------------------------------------------------------------------------------------------
+*
+*   This application implements a robot control program for the LynxMotion AL5D robot arm to pick up three coloured bricks,
+*   one red, one green, one blue, and stack them on top of each other, with the red brick on top and the blue brick at the base.
+*
+*   The filename of the robot configuration file is specificed on the first line of the input file.
+*   The filename of the camera model file is specified on the second line of the input file.
+*   The (x, y, z) position and orientation phi of the first brick in the stack is specified on the third line of the input file.
+*
+*   It is assumed that the x axis of the brick is aligned with its major axis. 
+*   It is also assumed that the dimensions of the brick in the x, y, and z directions are 31.8 mm,  15.8 mm, 11.4 mm respectively. 
+* 
+*   All poses – T5, gripper, and bricks – are specified in a Cartesian frame of reference using frames (i.e. homogenous transformations).
+*
+*   David Vernon
+*   5 April 2021
+*
+*   Audit Trail
+*   -----------
+*   Generated a amuhizi skeleton application by removing the core processing, i.e. brick segmentation, pose estimation in 2D,
+*   and pose estimation in 3D using the inverse perspective transformation
+*   David Vernon 8 April 2021
+*
+*******************************************************************************************************************/
 
-  1. The camera model for the camera
-  2. An image from the camera
+#include <stdlib.h>
+#include <time.h>
+#ifdef WIN32
+    #include "amuhizi.h"
+#else
+    #include <assignment5/amuhizi.h>
+#endif
 
-  It is assumed that the input file is located in a data directory given by the path ../data/ 
-  defined relative to the location of executable for this application.
 
-  It is also assumed that the images are in a data directory given by the path ../data/Media/
-  However, the camera model files are in a data directory given by the path ../data/ 
+int main(int argc, char ** argv) {
 
-  After computing the inverse perspective transformation, the user can then interactively select a point in the image.
-  The application then uses the inverse perspective transformation to compute the world x and y coordinates of the selected point.
-  It assumes the z coordinate is zero.
- 
- (This is the application file: it contains the client code that calls dedicated functions to implement the application.
-  The code for these functions is defined in the implementation file. The functions are declared in the interface file.)
+   /* initialize the ROS node  */
+   /* ------------------------ */
 
-  David Vernon
-  14 June 2018
-*/
+   /* we do this first in case one of the classes creates a node handle when the object is instantiated */
 
- 
-#include "assignment5/amuhizi.h"
-
-// Global variables to allow access by the display window callback functions
-
-Point2f image_sample_point; 
-int number_of_sample_points;
-Mat image;
-
-const char* window_name       = "Image";
-
-int main() {
-   
    #ifdef ROS
-      // Turn off canonical terminal mode and character echoing
-      static const int STDIN = 0;
-      termios term, old_term;
-      tcgetattr(STDIN, &old_term);
-      tcgetattr(STDIN, &term);
-      term.c_lflag &= ~(ICANON | ECHO);
-      tcsetattr(STDIN, TCSANOW, &term);
-   #endif 
-    
-   const char input_filename[MAX_FILENAME_LENGTH] = "cameraInvPerspectiveMonocularInput.txt";    
-   char input_path_and_filename[MAX_FILENAME_LENGTH];    
-   char data_dir[MAX_FILENAME_LENGTH];
-   char file_path_and_filename[MAX_FILENAME_LENGTH];
-     
+       ros::init(argc, argv, "amuhizi"); // Initialize the ROS system
+   #endif
 
-   int end_of_file;
-   bool debug = false;
-   char camera_model_filename[MAX_FILENAME_LENGTH];
-   char image_filename[MAX_FILENAME_LENGTH];
+   /* now come the declarations */
+
+   extern robotConfigurationDataType robotConfigurationData;
+   
+   bool debug = true;
+   
+   FILE *fp_in;                    // assignment 5 input file
+   FILE *fp_camera_model;          // camera model file
+   int  end_of_file; 
+   char robot_configuration_filename[MAX_FILENAME_LENGTH];
+   char camera_model_filename[MAX_FILENAME_LENGTH] = {};
+   char filename[MAX_FILENAME_LENGTH] = {};
+   char directory[MAX_FILENAME_LENGTH] = {};
+
+   const char* scene_window_name  = "Simulator Image";
+
+   /* Frame objects */
+   
+   Frame brick1;
+   Frame brick2;
+   Frame brick3;
+   Frame destination;
+   Frame E;
+   Frame T5;
+   Frame Z;
+   Frame home;
+   Frame out_of_view;
+
+   /* camera object for simulator */
+
+   SimulatorCamera camera;
+
+   /* image */
+
+   Mat frame;
+   
+   /* camera model */
+
+   float camera_model[3][4];
+
+   /* data variables */
+      
+   float destination_x;                    
+   float destination_y;
+   float destination_z;
+   float destination_phi;
+
+   float brick_height = 11;
+   
+   bool continuous_path = false;    // if true, implement approximation of continuous path control
+                                    // when approaching and departing the grasp pose
+                                    // otherwise just move directly from the initial approach pose to the grasp pose
+                                    // and directly from the grasp pose to the final depart pose
+   
+   int number_of_bricks = 0;        // number of brick detected in the scene
 
    int i, j;
-   float z;
 
-   Mat imageCopy;
-
-   Point3f world_sample_point;
-   Point2f text_coordinates; 
-
-   Scalar colour(0,255,0);
-
-   char coordinates[20];
-
-   FILE *fp_in;
-   FILE *fp_camera_model;
-
-   float    camera_model[3][4];
-
-   printf("Example of how to use openCV to compute the inverse perspective transformation.\n");
-   printf("Click on a point in the image to compute the world coordinates.\n\n");   
-   printf("Press any key to finish ...\n\n");
 
    
-   #ifdef ROS   
-      strcpy(data_dir, ros::package::getPath(ROS_PACKAGE_NAME).c_str()); // get the package directory
-   #else
-      strcpy(data_dir, "..");
-   #endif
+   /* open the input file */
+   /* ------------------- */
+
+   /* Set the filename; different directories for ROS and Windows versions */
    
-   strcat(data_dir, "/data/");
-   strcpy(input_path_and_filename, data_dir);
-   strcat(input_path_and_filename, input_filename);
+#ifdef ROS
+   strcat(directory, (ros::package::getPath(ROS_PACKAGE_NAME) + "/data/").c_str());
+#else
+   strcat(directory, "../data/"); // On Windows the exec is in bin, so we go in the parent directory first
+#endif
+
+   strcpy(filename, directory);
+   strcat(filename, "assignment5Input.txt"); // Input filename matches the application name
+
+   if ((fp_in = fopen(filename, "r")) == 0) {
+      printf("Error can't open input %s\n",filename);
+      prompt_and_exit(0);
+   }
+
    
+   /* get the robot configuration data */
+   /* -------------------------------- */
 
-   if ((fp_in = fopen(input_path_and_filename,"r")) == 0) {
-	  printf("Fatal error can't open input cameraInvPerspectiveMonocularInput.txt\n");
-     prompt_and_exit(1);
-   }
- 
-   end_of_file = fscanf(fp_in, "%s", camera_model_filename);
-   if (end_of_file == EOF) {
-      printf("Fatal error: unable to read camera model filename\n");
-     prompt_and_exit(1);
-   }
+   end_of_file = fscanf(fp_in, "%s", filename); // read the configuration filename   
 
-   end_of_file = fscanf(fp_in, "%s", image_filename);
-   if (end_of_file == EOF) {
-      printf("Fatal error: unable to read image filename\n");
-     prompt_and_exit(1);
-   }
-
-   /* get the left and right camera models */
-
-   if ((fp_camera_model = fopen(camera_model_filename,"r")) == 0) {
-	   printf("Error can't open camera model for input %s\n",camera_model_filename);
+   if (end_of_file == EOF) {   
+     printf("Fatal error: unable to read the robot configuration filename %s\n",filename);
       prompt_and_exit(1);
    }
-    
+
+   if (debug) printf("Robot configuration filename %s\n", filename);
+
+   strcpy(robot_configuration_filename, directory);
+   strcat(robot_configuration_filename, filename);
+
+   readRobotConfigurationData(robot_configuration_filename); // there is a dedicated function to read the configuration data
+                                                             // and store it in a global structure
+                                                            
+   
+   /* get the camera model data  */
+   /* -------------------------- */
+
+   end_of_file = fscanf(fp_in, "%s", filename); // read the configuration filename   
+
+   if (end_of_file == EOF) {   
+      printf("Fatal error: unable to read the camera model filename, %s\n", filename);
+      prompt_and_exit(1);
+   }
+
+   if (debug) printf("Camera model filename %s\n", filename);
+
+   strcpy(camera_model_filename, directory);
+   strcat(camera_model_filename, filename);
+
+   if ((fp_camera_model = fopen(camera_model_filename, "r")) == 0) {
+      printf("Error can't open input %s\n",camera_model_filename);
+      prompt_and_exit(0);
+   }
+
    for (i=0; i<3; i++) {
       for (j=0; j<4; j++) {
          fscanf(fp_camera_model, "%f ", &(camera_model[i][j]));
       }
    }
 
-   if ((fp_camera_model = fopen(camera_model_filename,"r")) == 0) {
-	   printf("Error can't open  camera model for input %s\n",camera_model_filename);
+   if (debug) {
+      printf("Camera model\n");
+      for (i=0; i<3; i++) {
+         for (j=0; j<4; j++) {
+            printf("%6.3f ", camera_model[i][j]);
+         }
+	 printf("\n");
+      }
+      printf("\n");
+   }
+
+   fclose(fp_camera_model);  
+
+   
+   /* get the destination pose data */
+   /* ----------------------------- */
+
+   end_of_file = fscanf(fp_in, "%f %f %f %f", &destination_x, &destination_y, &destination_z, &destination_phi);
+
+   if (end_of_file == EOF) {   
+	  printf("Fatal error: unable to read the destination position and orientation\n");
       prompt_and_exit(1);
    }
-    
-   for (i=0; i<3; i++) {
-      for (j=0; j<4; j++) {
-         fscanf(fp_camera_model, "%f ", &(camera_model[i][j]));
-      }
-   }
+   
+   if (debug) printf("Destination pose %6.3f %6.3f %6.3f %6.3f\n", destination_x, destination_y, destination_z, destination_phi);
 
-   /* get the image */
-
-   image = imread(image_filename, CV_LOAD_IMAGE_UNCHANGED);
-   if (image.empty()) {
-      cout << "can not open " << image_filename << endl;
-      prompt_and_exit(-1);
-   }
-
-   z = 0; // set the depth value for the inverse perspective transformation
-
-   /* Create a window for image and display it */
-   namedWindow(window_name, CV_WINDOW_AUTOSIZE );
-   setMouseCallback(window_name, getSamplePoint);    // use this callback to get the coordinates of the sample point
-   imshow(window_name, image);
-
-   /* now wait for user interaction - mouse click on image */
-   number_of_sample_points = 0;
-   do {
-      waitKey(30);   
-      if (number_of_sample_points == 1) {
-                                                         
-         inversePerspectiveTransformation(image_sample_point, camera_model, z, &world_sample_point);
-
-         text_coordinates.x = image_sample_point.x-7; // offset the graphic text message so that the + character is centred on the image sample point
-         text_coordinates.y = image_sample_point.y+4;
-
-         imageCopy  = image.clone(); 
-         sprintf(coordinates, "+ (%3.1f, %3.1f, %3.1f)", world_sample_point.x, world_sample_point.y, world_sample_point.z);
-         putText(imageCopy,  coordinates, text_coordinates, FONT_HERSHEY_SIMPLEX, 0.5, colour, 1 );
-         imshow(window_name, imageCopy);
-         number_of_sample_points =  0;  // reset to allow another sample
-      }
-   } while (!_kbhit());      
-         
-
-   destroyWindow(window_name);  
-
+   /* done reading the input file so close it */
+   
    fclose(fp_in);
-   fclose(fp_camera_model);
 
-   exit(0);
 
+      
+   /* now start the pick and place task */
+   /* --------------------------------- */
+
+
+   E    = trans((float) robotConfigurationData.effector_x,       // end-effector (gripper) frame
+		(float) robotConfigurationData.effector_y,       // is initialized from data
+		(float) robotConfigurationData.effector_z);      // in the robot configuration file
+   
+   Z    = trans(0.0, 0.0, 0.0);                                  // robot base frame
+
+   /* move to the home pose                                                   */
+   /*                                                                         */
+   /* Define the home pose with the wrist T6 positioned over theworls  Y axis */
+   /* so that link a3 is vertical and link a4 is  horizontal                  */
+   /* with the approach vector directed along the world  Y axis               */
+   /* and the orientation vector directed aloing the the X axis               */
+   /* thus, the normal vector is directed in the - Z direction                */
+   
+   home = trans(0, 187, 216) *  roty(90.0) * rotx(-90);
+   
+   T5   = inv(Z) * home; // no need to include inv(E) here since we are specifying the wrist pose directly
+
+   if (move(T5) == false) display_error_and_exit("move error ... quitting\n");
+
+   wait(3000);
+
+   /* move out of the field of view of the camera                                    */
+   /*                                                                                */
+   /* Define the out_of_view pose with the wrist T6 positioned over the world X axis */
+   /* so that link a3 is vertical and link a4 is  horizontal                         */
+   /* with the approach vector directed along the world  -Z axis                     */
+   /* and the orientation vector directed in the  with the -Y axis                   */
+   /* thus, the normal vector is directed in the - X direction                       */
+   
+   out_of_view = trans(187, 0, 216) * roty(180.0);
+
+   T5   = inv(Z) * out_of_view; // no need to include inv(E) here since we are specifying the wrist pose directly
+
+   if (move(T5) == false) display_error_and_exit("move error ... quitting\n");
+
+   wait(3000);
+
+
+   /* create a window to see the acquired image */
+   
+   namedWindow(scene_window_name, CV_WINDOW_AUTOSIZE);
+   waitKey(30);
+   moveWindow(scene_window_name, 0, 0);
+   waitKey(30);
+
+   /* initialize the camera */
+
+   camera.initialize();
+   
+   /* acquire an image */
+
+   camera.getImage(frame);
+   imshow (scene_window_name, frame);
+   waitKey(3000);  // three second delay so that we can see the image before the window is destroyed.
+          
+   destroyWindow(scene_window_name);
+
+   
+   /* analyze the image to determine the pose of the three bricks in the world or robot frame of reference */
+   /* sort the poses by increasing value of  hue                                                           */
+   /* ---------------------------------------------------------------------------------------------------- */
+
+   
+   /* insert your code here */
+
+
+   
+   /* stack the bricks: red on top, green in the middle, and blue at the bottom  */
+   /* -------------------------------------------------------------------------  */
+
+
+   /* insert your code here */
+
+   
+   
+   return 0;
 }
-
