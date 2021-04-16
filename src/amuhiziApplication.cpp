9685a09fc035467d00c68b6855b0f846b61d2522
Muhizi Aristide <muhizi@Muhizis-MacBook-Pro.local>
2021-04-16 12:46:41 +0200

    removed a double definition function

--- src/amuhiziApplication.cpp
+++ src/amuhiziApplication.cpp
@@ -351,7 +351,6 @@ int main(int argc, char ** argv) {
     for (int i = 0; i < NUM_BRICKS; i++) {
         pick_and_place(bricks_pose[i][0], bricks_pose[i][1], bricks_pose[i][2], bricks_pose[i][3], destination_x, destination_y, bricks_dest_z[i], destination_phi, 0, 0, 5, 180);
     }
-   pick_and_place(bricks_pose[2][0], bricks_pose[2][1], bricks_pose[2][2], bricks_pose[2][3], -60, destination_y, bricks_dest_z[i], destination_phi, 0, 0, 5, 180);
 
     camera.getImage(frame);
     T5   = inv(Z) * out_of_view; // no need to include inv(E) here since we are specifying the wrist pose directly
