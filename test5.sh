a7598a50f0967a3db8048b422e24b9e233bb42e3
Muhizi Aristide <muhizi@172-29-104-196.w.africa.local.cmu.edu>
2021-04-27 08:23:48 +0200

    new test

---
+++ test5.sh
@@ -0,0 +1,3 @@
+rosrun lynxmotion_al5d_description spawn_brick -n brick1 -c red   -x -0.05 -y 0.10 -z 0 -Y 0
+rosrun lynxmotion_al5d_description spawn_brick -n brick2 -c green -x  0.00 -y 0.15 -z 0 -Y 0.5
+rosrun lynxmotion_al5d_description spawn_brick -n brick3 -c blue  -x  0.07 -y 0.20 -z 0 -Y 1.0
